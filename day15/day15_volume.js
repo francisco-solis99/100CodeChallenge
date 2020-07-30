30712aea81714a06be835b1836ce9d5a6e4ac8d5
Francisco Solis <franciscosoliscss@gmail.com>
2020-07-30 18:32:36 -0500

    Agregando info necesaria de las formulas para el reto

--- day15/day15_volume.js
+++ day15/day15_volume.js
@@ -4,5 +4,11 @@
 
     Las matemáticas son base fundamental de la lógica y programación, por eso es importante practicarlas constantemente. Un cilindro es un cuerpo geométrico que requiere de varias fórmulas, aplícalas en un programa que reciba datos como su altura y radio de las bases para mostrar el resultado acotado a un decimal.
 
+
+    Cilindro -> pi * radio * radio * altura
+    Cubo -> Lado * Lado * Lado
+    Esfera ->  4/3 * pie * radio * radio * radio
+
 */
+
 
