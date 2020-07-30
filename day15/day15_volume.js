1ffa29bb79740927c74ebed5310afeec46d36e04
Francisco Solis <franciscosoliscss@gmail.com>
2020-07-30 18:26:26 -0500

    Vinculando el index con el js

--- day15/day15_volume.js
+++ day15/day15_volume.js
@@ -1,7 +1,7 @@
 /*
     Reto dia 15:
     Debe elegir entre Cilindro, Cubo y Esfera
-    
+
     Las matemáticas son base fundamental de la lógica y programación, por eso es importante practicarlas constantemente. Un cilindro es un cuerpo geométrico que requiere de varias fórmulas, aplícalas en un programa que reciba datos como su altura y radio de las bases para mostrar el resultado acotado a un decimal.
 
 */
