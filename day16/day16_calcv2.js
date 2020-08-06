d4448db68080e4734ef773ddb988717f017df9cc
Francisco Solis <franciscosoliscss@gmail.com>
2020-08-06 12:40:39 -0500

    Agregados archivos index, estilos y vinculado el css y el js con el html

--- day16/day16_calcv2.js
+++ day16/day16_calcv2.js
@@ -2,9 +2,11 @@
     Dia 16 
     Te diste cuenta de que muchas veces hacemos cálculos que son con más de dos valores, quiero que crees el algoritmo para programar una calculadora a la que le puedas agregar tantos números como tú requieras, deberá servir para adiciones y multiplicaciones
 
-    
-
-
-
-
 */
+
+
+
+
+
+
+
