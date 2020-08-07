6107d91da3024a3c745cff43ffc0a45de6421497
Francisco Solis <franciscosoliscss@gmail.com>
2020-08-07 11:39:01 -0500

    Funcionalidades de la calculadora en js programadas

--- day16/day16_calcv2.js
+++ day16/day16_calcv2.js
@@ -4,30 +4,75 @@
 
 */
 
+    //Capturando botones
     let bottons = [];
     for(let i = 0; i < 10; i += 1){
         bottons[i] = document.querySelector(`.calc__${i}`);
     }
 
+    //capturando operadores o signos
     const suma = document.querySelector('.calc__sum');
     const mult = document.querySelector('.calc__mult');
-    const ce = document.querySelector('.calc__ce');
-    const  resultado = document.querySelector('.pantalla');
-
+    const ce = document.querySelector('.calc_ce');
+    const  display = document.querySelector('.pantalla');
+    const  result = document.querySelector('.calc__equal');
+    const  dot = document.querySelector('.calc__dot');
     
+
+    //BOTONES
     for(let i = 0; i < bottons.length; i+=1){
         bottons[i].onclick  = () => {
-            resultado.innerHTML += i;
+            display.innerHTML += i;
         }
     }
 
+    //SUMA
+    suma.onclick  = () => {
+        // let cadena = display.textContent;
+         // console.log(cadena[cadena.length - 1]);
+
+        if(display.textContent[display.textContent.length - 1] === '*') {
+            alert("Un signo a la vez");
+        }
+        else{
+            display.innerHTML += '+';
+        }
+    }
+
+    //MULTIPLICACION
+    mult.onclick  = () => {
+        if(display.textContent[display.textContent.length - 1] === '+') {
+            alert("Un signo a la vez");
+        }
+        else{
+            display.innerHTML += '*';
+        }
+    }
+
+    //BORRAR TODO
+    ce.onclick  = () => {
+        display.textContent = '';
+    }
+
+    //PUNTO DECIMAL
+    dot.onclick  = () => {
+        display.textContent += '.';
+    }
+    
+    //RESULTADO EN PANTALLA
+    result.onclick  = () => {
+        console.log(eval(display.textContent));
+        display.innerHTML = eval(display.textContent).toFixed(3); //a tres decimales solamente
+    }
+
+    
 
 
     // queyselector !== getelementByClassName
 
 // console.log(bottons);
 // console.log(suma);
-// console.log(mult);
+
 // console.log(resultado);
 
 
